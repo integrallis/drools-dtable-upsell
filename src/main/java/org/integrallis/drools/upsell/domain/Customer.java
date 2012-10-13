82e20d5c42d3fe5a5a6f604329b4e4924b1788df
Brian Sam-Bodden <bsbodden@integrallis.com>
2012-10-13 08:04:21 -0700

    Cleaned up imports

--- src/main/java/org/integrallis/drools/upsell/domain/Customer.java
+++ src/main/java/org/integrallis/drools/upsell/domain/Customer.java
@@ -3,7 +3,6 @@ package org.integrallis.drools.upsell.domain;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class Customer {
