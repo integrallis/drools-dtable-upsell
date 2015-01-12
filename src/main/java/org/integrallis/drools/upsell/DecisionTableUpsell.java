be10dfad50b095d9f5eeea5a45940860d00b3911
Brian Sam-Bodden <bsbodden@integrallis.com>
2015-01-11 21:15:36 -0700

    Updated to work with Drools 6.1.0

--- src/main/java/org/integrallis/drools/upsell/DecisionTableUpsell.java
+++ src/main/java/org/integrallis/drools/upsell/DecisionTableUpsell.java
@@ -1,21 +1,11 @@
 package org.integrallis.drools.upsell;
 
-import org.drools.KnowledgeBase;
-import org.drools.KnowledgeBaseFactory;
-import org.drools.builder.DecisionTableConfiguration;
-import org.drools.builder.DecisionTableInputType;
-import org.drools.builder.KnowledgeBuilder;
-import org.drools.builder.KnowledgeBuilderError;
-import org.drools.builder.KnowledgeBuilderErrors;
-import org.drools.builder.KnowledgeBuilderFactory;
-import org.drools.builder.ResourceType;
-import org.drools.io.ResourceFactory;
-import org.drools.logger.KnowledgeRuntimeLogger;
-import org.drools.logger.KnowledgeRuntimeLoggerFactory;
-import org.drools.runtime.StatefulKnowledgeSession;
 import org.integrallis.drools.upsell.domain.Customer;
 import org.integrallis.drools.upsell.domain.DateUtil;
 import org.integrallis.drools.upsell.domain.Product;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
 
 /**
  * This is a sample class to launch a decision table.
@@ -23,41 +13,28 @@ import org.integrallis.drools.upsell.domain.Product;
 public class DecisionTableUpsell {
 
     public static final void main(String[] args) {
-        try {
-            // load up the knowledge base
-            KnowledgeBase kbase = readKnowledgeBase();
-            StatefulKnowledgeSession ksession = kbase.newStatefulKnowledgeSession();
-            KnowledgeRuntimeLogger logger = KnowledgeRuntimeLoggerFactory.newFileLogger(ksession, "test");
+	    KieSession knowledgeSession = null;
+	    try {
+	    		// load up the knowledge base
+	    		KieServices ks = KieServices.Factory.get();
+	        KieContainer kContainer = ks.getKieClasspathContainer();
+	        knowledgeSession = kContainer.newKieSession("ksession-rules");
+			
             // go !
             Customer customer = new Customer("Brian Sam-Bodden", 1300.00, DateUtil.getDate("2007-06-21"));
             customer.addProducts(Product.CHECKING_ACCOUNT);
-            ksession.insert(customer);
-            ksession.fireAllRules();
-            logger.close();
+            knowledgeSession.insert(customer);
+            knowledgeSession.fireAllRules();
             System.out.println(customer);
             for (Product product : customer.getRecommended()) {
 				System.out.println("Recommended Product ===> " + product);
 			}
         } catch (Throwable t) {
             t.printStackTrace();
+        } finally {
+			knowledgeSession.dispose();
         }
     }
 
-    private static KnowledgeBase readKnowledgeBase() throws Exception {
-        KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
-        DecisionTableConfiguration config = KnowledgeBuilderFactory.newDecisionTableConfiguration();
-        config.setInputType(DecisionTableInputType.XLS);
-        kbuilder.add(ResourceFactory.newClassPathResource("UpsellDTRules.xls"), ResourceType.DTABLE, config);
-        KnowledgeBuilderErrors errors = kbuilder.getErrors();
-        if (errors.size() > 0) {
-            for (KnowledgeBuilderError error: errors) {
-                System.err.println(error);
-            }
-            throw new IllegalArgumentException("Could not parse knowledge.");
-        }
-        KnowledgeBase kbase = KnowledgeBaseFactory.newKnowledgeBase();
-        kbase.addKnowledgePackages(kbuilder.getKnowledgePackages());
-        return kbase;
-    }
 
 }
